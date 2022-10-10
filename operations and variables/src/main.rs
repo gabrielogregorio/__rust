7981dd7776c5ae5823fb8dc0de8847ea30f7c449
gabriel <gabriel.gregorio.1@outlook.com>
2022-10-10 12:41:27 -0300

    added variables and settings

--- operations and variables/src/main.rs
+++ operations and variables/src/main.rs
@@ -1,8 +1,14 @@
+const MY_CONST_ABC: &str = "santos";
+
 fn main() {
+    let qualquerVariael = "abc, faz inferenia";
+    let qualquerVariael2: &str = "abc, faz inferenia2";
+
+    println!("{}, {} {}", qualquerVariael, qualquerVariael2, MY_CONST_ABC);
     println!("logic1 {}", !true);
     println!("logic2! {}", !true || false);
     println!("logic3! {}", !true && false || true);
-    
+
     println!("Integer positive only {}", 450u32 - 200);
     println!("integer real only {}", 450i32 - 200);
     println!("integer real only {}", 10i32 - 200);
